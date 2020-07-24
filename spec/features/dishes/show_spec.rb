3eb529ff6a210ff1e828b7490341bd942fa730b4
Norma Lopez <norma_l304@yahoo.com>
2020-07-24 09:29:38 -0600

    Test dish show page displays ingredients and chef name

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Dish show page' do
+  it 'displays list of ingredients and chef name' do
+    jackie = Chef.create!(name: "Jackie Casas")
+    ravioli = jackie.dishes.create!(name: "Ravioli", description: "Spinach and cheese ravioli.")
+    cheese = Ingredient.create!(name: "Cheese", calories: 85)
+    spinach = Ingredient.create!(name: "Spinach", calories: 30)
+    pasta = Ingredient.create!(name: "Pasta", calories: 120)
+
+    DishIngredient.create!(dish: ravioli, ingredient: cheese)
+    DishIngredient.create!(dish: ravioli, ingredient: spinach)
+    DishIngredient.create!(dish: ravioli, ingredient: pasta)
+
+    visit "dishes/#{ravioli.id}"
+
+    expect(page).to have_content("Cheese")
+    expect(page).to have_content("Spinach")
+    expect(page).to have_content("Pasta")
+    expect(page).to have_content("Jackie Casas")
+  end
+end
