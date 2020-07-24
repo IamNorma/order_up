6c5f565abdf21c562be881aad0c6872b32141f1b
Norma Lopez <norma_l304@yahoo.com>
2020-07-24 09:53:35 -0600

    Add model test for #total_calories

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe Dish, type: :model do
   describe "relationships" do
     it {should belong_to :chef}
   end
+
+  describe "methods" do
+    it '#total_calories' do
+      jackie = Chef.create!(name: "Jackie Casas")
+      ravioli = jackie.dishes.create!(name: "Ravioli", description: "Spinach and cheese ravioli.")
+      cheese = Ingredient.create!(name: "Cheese", calories: 85)
+      spinach = Ingredient.create!(name: "Spinach", calories: 30)
+      pasta = Ingredient.create!(name: "Pasta", calories: 120)
+
+      DishIngredient.create!(dish: ravioli, ingredient: cheese)
+      DishIngredient.create!(dish: ravioli, ingredient: spinach)
+      DishIngredient.create!(dish: ravioli, ingredient: pasta)
+
+      expect(ravioli.total_calories).to eq(235)
+    end
+  end
 end
